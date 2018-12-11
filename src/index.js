1520473a6d9395baea0d133f480900e8b3af37a8
Dams-d <damien.duro@epitech.eu>
2018-12-11 20:30:58 +0000

    init section files

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import section from './sectionNewsletterA.vue'
+
+const name = "newsletter_A";
+
+const addComponent = function () {
+    if (window.vm) {
+        window.vm.addComponent(name, section);
+
+        return true;
+    }
+    return false;
+}
+
+if (!addComponent()) {
+    const iniInterval = setInterval(function () {
+        if (addComponent()) {
+            clearInterval(iniInterval);
+        }
+    }, 10);
+}
